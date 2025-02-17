3f9cf559a93cdd21fd39abad67d8b25b7aeaf4a7
fridasorken <frida.sorken@gmail.com>
2025-02-17 18:02:08 +0100

    makes a shared queue in blackboard to save waypoints to

--- src/bt_drone/src/search_start.cpp
+++ src/bt_drone/src/search_start.cpp
@@ -1,4 +1,5 @@
 #include "bt_drone/search_start.hpp"
+#include <deque>
 
 SearchStart::SearchStart(const std::string& name, const BT::NodeConfig& config)
     : SyncActionNode(name,config)
@@ -10,12 +11,12 @@ BT::PortsList SearchStart::providedPorts()
              BT::InputPort<int>("area_width"),
              BT::InputPort<int>("drone_id"),
              BT::BidirectionalPort<NED>("search_start_pose"),
-             BT::OutputPort<NED>("search_end_vector"),
-             BT::OutputPort<NED>("search_path_vector") };
+             BT::OutputPort<BT::SharedQueue<NED>>("lawnmower_queue")};
 }
 
 BT::NodeStatus SearchStart::tick()
 {
+    auto lawnmower_queue = std::make_shared<std::deque<NED>>();
     BT::Expected<int> drone_id = getInput<int>("drone_id");
     BT::Expected<int> area_height = getInput<int>("area_height");
     BT::Expected<int> area_width = getInput<int>("area_width");
@@ -30,23 +31,28 @@ BT::NodeStatus SearchStart::tick()
     }
 
     NED search_start_pose;
-    NED search_end_vector;
-    NED search_path_vector;
+    NED search_side_vector;
+    NED search_up_path_vector;
+    NED search_down_path_vector;
 
     //First search
     if (!start_pose)
     {
-        search_start_pose = {0, double(0 + drone_id.value()), -6};
-        search_end_vector = {0, double(4 + drone_id.value()), 0};
-        search_path_vector = {double(area_height.value()), 0, 0};
+        search_start_pose = {0, double(0 + drone_id.value()), 0};
+        search_up_path_vector = {double(area_height.value()), 0, 0};
+        search_side_vector = {double(area_height.value()), double(4 + drone_id.value()), 0};
+        search_down_path_vector = {0, double(4 + drone_id.value()), 0};
+        
         
         std::cout << "Initializing new search_start_pose: " << search_start_pose << std::endl;
 
     } else { //Has already done lawnmower
         
-        search_start_pose = {0, start_pose.value().east + 8 + drone_id.value(), -6};
-        search_end_vector = {0, 4 + search_start_pose.east, 0};
-        search_path_vector = {double(area_height.value()), search_start_pose.east, 0};
+        search_start_pose = {0, start_pose.value().east + 8 + drone_id.value(), 0};
+        search_up_path_vector = {double(area_height.value()), start_pose.value().east + 8 + drone_id.value(), 0};
+        search_side_vector = {double(area_height.value()), start_pose.value().east + 8 + drone_id.value() + 4, 0};
+        search_down_path_vector = {0, start_pose.value().east + 8 + drone_id.value() + 4, 0};
+        
         
         std::cout << "Using existing start_pose to compute new values: " << search_start_pose << std::endl;
     }
@@ -55,8 +61,14 @@ BT::NodeStatus SearchStart::tick()
     if (!success) {
         std::cerr << "Failed to set search_start_pose in blackboard!" << std::endl;
     }
-    setOutput("search_end_vector", search_end_vector);
-    setOutput("search_path_vector", search_path_vector);
+
+    lawnmower_queue->push_back(search_start_pose);
+    lawnmower_queue->push_back(search_up_path_vector);
+    lawnmower_queue->push_back(search_side_vector);
+    lawnmower_queue->push_back(search_down_path_vector);
+
+    setOutput("lawnmower_queue", lawnmower_queue);
+
     
     return BT::NodeStatus::SUCCESS; 
 }
